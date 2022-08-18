58328cb3daaf5f6a5e28cd5c56b46900f3e1e8ed
Daniel-online <86497631+Daniel-online@users.noreply.github.com>
2022-08-18 14:36:09 -0300

    Create questao25.c

---
+++ Trabalho Alg-Maria/questao25.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main (void )
+{
+int horasEx, horasFal;
+float bonus, min;
+
+printf("Insira o número de horas trabalhadas além do expediente normal..:\n");
+scanf("%d",&horasEx);
+printf("Insira número de horas m que o funcionário esteve ausente..:\n");
+scanf("%d",&horasFal);
+
+bonus= horasEx-(2/3*(horasFal));
+min = bonus * 60;
+
+
+if(bonus>2400){
+    printf("Bonus é de R$ 500,00");
+}
+else if(bonus>=1800 && bonus<2400){
+    printf("Bonus é de R$ 400,00");
+}
+else if(bonus>=1200 && bonus>1800){
+    printf("Bonus é de R$ 300,00");
+}
+else if(bonus>=600 && bonus>1200){
+    printf("Bonus é de R$ 200,00");
+}
+else if(bonus<600){
+    printf("Bonus é de R$ 100,00");
+}
+
+}
